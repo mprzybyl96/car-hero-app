6b43d5b8b3ae3b5d73fa15e2013ecddfa4c378ce
maciejprzybyl-sa <maciej.przybyl@shop-apotheke.com>
2023-07-28 15:05:57 +0200

    added landing page

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,7 +1,24 @@
-import React from "react";
+"use client";
 
-const CustomButton = () => {
-  return <div>CustomButton</div>;
+import React from "react";
+import Image from "next/image";
+import { ICustomButton } from "@/types";
+
+const CustomButton = ({
+  title,
+  containerStyles,
+  handleClick,
+}: ICustomButton) => {
+  return (
+    <button
+      disabled={false}
+      type="button"
+      className={`custom-btn ${containerStyles}`}
+      onClick={handleClick}
+    >
+      <span className="flex-1 ">{title}</span>
+    </button>
+  );
 };
 
 export default CustomButton;
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,12 @@
+"use client";
+
 import React from "react";
 import CustomButton from "./CustomButton";
+import Image from "next/image";
 
 const Hero = () => {
+  const handleScroll = () => {};
+
   return (
     <div className="hero">
       <div className="flex-1 pt-36 padding-x">
@@ -12,7 +17,17 @@ const Hero = () => {
           Streamline your car rental experience without effortless booking
           process.
         </p>
-        <CustomButton />
+        <CustomButton
+          title="Explore Cars"
+          containerStyles="bg-primary-blue text-white rounded-full mt-10"
+          handleClick={handleScroll}
+        />
+      </div>
+      <div className="hero__image-container">
+        <div className="hero__image">
+          <Image src="/hero.png" alt="hero" className="object-contain" fill />
+          <div className="hero__image-overlay"></div>
+        </div>
       </div>
     </div>
   );
